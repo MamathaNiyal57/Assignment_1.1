35988ff74c7b494f1c215dd9ef68afd7dd279e68
Admin <admin@Admins-MacBook-Pro-25.local>
2024-02-20 12:35:44 +0530

    second commit

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,19 @@
-#Assignment_1
+import java.util.*;
+import java.io;
+
+public class Calculator
+{
+    
+    public static void main(String args[])
+    {
+      Scanner s =new Scanner(System.in);
+      int a;
+      int b;
+      a=s.nextInt();
+      b=s.nextInt();
+
+        
+    }
+}
+
+
